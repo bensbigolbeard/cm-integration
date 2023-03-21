da8e53c0a3d9ff168cb0b6d53ca242ebb154d844
Ben White <bensbigolbeard@gmail.com>
2023-03-20 21:01:36 -0500

    chore: update dependencies

--- faq-questions.js
+++ faq-questions.js
@@ -1,6 +1,7 @@
 const CATEGORIES = {
   WEN: "Wen",
   DROPS: "Drops",
+  ACCESS: "Access",
 };
 const FAQs = [
   {
@@ -13,7 +14,19 @@ const FAQs = [
     name: "Is there any point in hodling Crime Reports?",
     category: CATEGORIES.DROPS,
     value:
-      "No, beyond pure speculation. You can't stay on the run *and* hang out with everyone in the clink.",
+      "No, beyond pure price speculation. You can't stay on the run *and* hang out with everyone in the clink.",
+  },
+  {
+    name: "I have a Cel Mate, now what?",
+    category: CATEGORIES.ACCESS,
+    value:
+      "If you haven't already, fill out your #paperwork to get full access to the rest of the server. You can also start accumulating Steaks, by putting them on Death Row (#clink-links), which will be required for all future drops in the Hanging Hall.",
+  },
+  {
+    name: "What's the Hanging Hall?",
+    category: CATEGORIES.ACCESS,
+    value:
+      "It's where all future Cel Mate art collabs will be raffled and/or sold. Mcbess also releases solo works here, directly to the community, including new editions of his best known collection, Les Ladies.",
   },
 ];
 
